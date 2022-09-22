b2517e2769a0ed973e8b89621676a5fbcd880670
Ed Ropple <ed@edropple.com>
2022-09-22 13:02:44 -0400

    windows tests (currently repeating dummyprovider test)

--- providers/windows_provider_test.ts
+++ providers/windows_provider_test.ts
@@ -1,8 +1,28 @@
+import "../test_helpers/log.ts";
 import { Asserts } from "../dev_deps.ts";
+import { WindowsProvider } from "./windows_provider.ts";
 
-if (Deno.build.os === "windows") {
-  Deno.test("in/out test", () => {
-    Asserts.assert(false);
-  });
-}
+Deno.test("in/out test", async () => {
+  const t = await Deno.makeTempFile();
+  try {
+    await Deno.remove(t);
+    const provider = new WindowsProvider({
+      file: t,
+    });
 
+    const key = `dummykey-${Math.ceil(Math.random() * 1000)}`;
+    const value = "my deno test value";
+
+    Asserts.assertEquals(await provider.getValue(key), null);
+
+    await provider.setValue(key, value);
+
+    Asserts.assertEquals(await provider.getValue(key), value);
+
+    await provider.deleteValue(key);
+
+    Asserts.assertEquals(await provider.getValue(key), null);
+  } finally {
+    await Deno.remove(t);
+  }
+});
--- providers/wsl2_provider_test.ts
+++ providers/wsl2_provider_test.ts
@@ -1,8 +1,30 @@
+import "../test_helpers/log.ts";
 import { Asserts } from "../dev_deps.ts";
+import { WSL2Provider } from "./wsl2_provider.ts";
 
 if (Deno.build.os === "linux" && Deno.env.get("WSL_DISTRO_NAME")) {
-  Deno.test("in/out test", () => {
-    Asserts.assert(true);
+  Deno.test("in/out test", async () => {
+    const t = await Deno.makeTempFile();
+    try {
+      await Deno.remove(t);
+      const provider = new WSL2Provider({
+        file: t,
+      });
+
+      const key = `dummykey-${Math.ceil(Math.random() * 1000)}`;
+      const value = "my deno test value";
+
+      Asserts.assertEquals(await provider.getValue(key), null);
+
+      await provider.setValue(key, value);
+
+      Asserts.assertEquals(await provider.getValue(key), value);
+
+      await provider.deleteValue(key);
+
+      Asserts.assertEquals(await provider.getValue(key), null);
+    } finally {
+      await Deno.remove(t);
+    }
   });
 }
-
